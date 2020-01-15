f2ca078f65f423edc70740934a1806ae68bccdf4
yukimuneee <norugbynolife9@icloud.com>
2020-01-15 19:54:37 +0900

    setting

--- app/controllers/personals_controller.rb
+++ app/controllers/personals_controller.rb
@@ -2,6 +2,5 @@ class PersonalsController < ApplicationController
   def index 
   end
 
-  def first_page
-  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users
+  # devise_for :users, controllers: {
+  #   registrations: 'users/registrations'
+
   root to: 'personals#index'
+
   resources :personals do
     collection do
       get 'index'
       get 'first_page'
-      
     end
   end
 end
