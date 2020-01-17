6d7fcf17e66d61f1e9fcef88eb747828c9e8f93c
yukimuneee <norugbynolife9@icloud.com>
2020-01-17 15:48:09 +0900

    setting

--- app/assets/javascripts/first_pagae.js
+++ app/assets/javascripts/first_pagae.js
@@ -1,7 +1,7 @@
 $(function(){
   $('.fmain__images').slick({
     autoplay: true,
-    autoplaySpeed: 3000,
+    autoplaySpeed: 5000,
     dots: true,
     infinite: true,
     speed: 500,
--- app/assets/javascripts/password.js
+++ app/assets/javascripts/password.js
@@ -1,12 +1,12 @@
-// $(window).on("turbolinks:load", function() {
-//   var password  = '#js-password';
-//   var passcheck = '#js-passcheck';
+$(window).on("turbolinks:load", function() {
+  var password  = '#js-password';
+  var passcheck = '#js-passcheck';
   
-//   $(passcheck).change(function() {
-//       if ($(this).prop('checked')) {
-//           $(password).attr('type','text');
-//       } else {
-//           $(password).attr('type','password');
-//       }
-//   });
-// });
+  $(passcheck).change(function() {
+      if ($(this).prop('checked')) {
+          $(password).attr('type','text');
+      } else {
+          $(password).attr('type','password');
+      }
+  });
+});
