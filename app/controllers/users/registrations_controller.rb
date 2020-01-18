5ce9c105b23bdd4c54be0eeda0a5d2fb81913f04
yukimuneee <norugbynolife9@icloud.com>
2020-01-18 22:02:45 +0900

    setting

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,71 +2,86 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
 
- def index
- end
+  def index
+  end
 
- def new
-    @user = User.new
- end
+  def new
+      @user = User.new
+  end
 
- def create
-    session[:nickname] = user_params[:nickname]
-    session[:email] = user_params[:email]
-    session[:password] = user_params[:password] 
+  def create
+      session[:nickname] = user_params[:nickname]
+      session[:email] = user_params[:email]
+      session[:password] = user_params[:password] 
 
+      @user = User.new(
+        nickname: session[:nickname],
+        email: session[:email],
+        password: session[:password],
+      )
+
+      check_user_valid = @user.valid?
+      unless check_user_valid
+        render :new
+      else
+        session[:through_first_valid] = "through_first_valid"
+        redirect_to additional_path
+      end
+  end
+
+  def new_additional
+      @user = User.new
+      @personal = Personal.new
+  end
+
+  def create_additional
     @user = User.new(
       nickname: session[:nickname],
       email: session[:email],
       password: session[:password],
     )
+    @user.save 
+    
+    @personal = Personale.new(
+      height: session[:height],
+      weight: session[:weight],
+      age: session[:age],
+      month: session[:month],
+      monthly_drinking_money: session[:monthly_drinking_money]
+    )
 
-    check_user_valid = @user.valid?
-    unless check_user_valid
-      render :new
+    if @personal.save
+      reset_session
+      session[:id] = @user.id
+      redirect_to done_path
+      return 
     else
-      session[:through_first_valid] = "through_first_valid"
-      redirect_to additional_path
+      reset_session
+      redirect_to new_path
     end
- end
 
- def new_additional
-    @user = User.new
-    @personal = Personal.new
- end
-
- def create_additional
-  session[:height] = personal_params[:height]
-  session[:weight] = personal_params[:weight]
-  session[:age] = personal_params[:age]
-  session[:month] = personal_params[:month]
-  session[:monthly_drinking_money] = personal_params[:monthly_drinking_money]
-
-  @user = User.new(
-    nickname: session[:nickname],
-    email: session[:email],
-    password: session[:password],
-  )
-
-  @personal = Personale.new(
-    height: session[:height],
-    weight: session[:weight],
-    age: session[:age],
-    month: session[:month],
-    monthly_drinking_money: session[:monthly_drinking_money]
-  )
-
-  check_user_valid = @user.valid?
-  check_personal_valid = @personal.valid?
-  unless check_user_valid && check_personal_valid
-    render :new_additional
-  else
-    session[:through_first_valid] = "through_first_valid"
-    redirect_to done_path
   end
- end
 
+  def done
+    unless session[:id]
+      redirect_to root_path 
+      return
+    end
+    sign_in User.find(session[:id])
+  end
 
- def done
- end
+  private
+    
+  def user_params
+    params.require(:user).permit(:nickname, :email, :password)
+  end
 
+  def cellphone_params
+    params.require(:personal).permit(:height, :weight, :age, :month, :monthly_drinking_money)
+  end
+
+  # 前のpostアクションで定義されたsessionがなかった場合登録ページトップへリダイレクト
+  def redirect_to_index_from_sms
+    redirect_to signup_index_path unless session[:through_first_valid].present? && session[:through_first_valid] == "through_first_valid"
+  end
 end
