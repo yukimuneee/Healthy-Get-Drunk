41bc659c48df9fea42d46e5990d4b7f70a12af75
yukimuneee <norugbynolife9@icloud.com>
2020-01-21 17:12:05 +0900

    seeting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,6 @@
 class UsersController < ApplicationController
 
   def edit
-    user = User.find(current_user.id)
-    @nickname = user.nickname
-    @height = user.personal.height
-    @weight = user.personal.weight
-    @age = user.personal.age
-    @month = user.personal.month
-    @monthly_drinking_money = user.personal.monthly_drinking_money
   end
 
   def update
