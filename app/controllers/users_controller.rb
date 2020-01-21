5a5690b8fc30cba61779bc209f907a5b74eca2fc
yukimuneee <norugbynolife9@icloud.com>
2020-01-21 17:53:10 +0900

    setting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,12 @@
 class UsersController < ApplicationController
 
   def edit
+    @user = User.find(params[:id]) 
   end
 
-  def update
-    user = User.find(params[:id])
-    user.update(user_params)
-  end
+  # def update
+  #   user = User.find(params[:id])
+  #   user.update(user_params)
+  # end
 end
 
