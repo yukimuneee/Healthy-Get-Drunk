5ed726ada86a84f9c8b8e4e43021208ad5f29c16
yukimuneee <norugbynolife9@icloud.com>
2020-01-22 19:27:43 +0900

    server

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,16 @@ class UsersController < ApplicationController
     @user = User.find(current_user.id)
   end
 
-  # def update
-  #   user = User.find(params[:id])
-  #   user.update(user_params)
-  # end
+  def update
+    @user = User.find(current_user.id)
+    if @user.update(update_user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
 end
 
