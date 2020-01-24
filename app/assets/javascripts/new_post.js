8194075a94bf0150bcf673567ebf4178eaaaa0db
yukimuneee <norugbynolife9@icloud.com>
2020-01-24 16:52:31 +0900

    setting

--- app/assets/javascripts/new_post.js
+++ app/assets/javascripts/new_post.js
@@ -1,5 +1,5 @@
 $(function(){
-  $('#price').on('input',function(){
+  $('#money').on('input',function(){
     var value = $(this).val();
     if(value > 9999999){
       $('#fee').val('-');
@@ -11,7 +11,32 @@ $(function(){
     }
   });
 
-  $('#price').on('input',function(){
+  $('#money').on('input',function(){
+    var value = $(this).val();
+    if(value > 9999999){
+      $('#profit').val('-');
+    } else if(300 > value){
+      $('#profit').val('-');
+    } else{
+      var fee = value / 10;
+      var profit = value - fee;
+      $('#profit').val(profit);
+    }
+  });
+
+  $('#weight').on('input',function(){
+    var value = $(this).val();
+    if(value > 9999999){
+      $('#fee').val('-');
+    } else if(300 > value){
+      $('#fee').val('-');
+    } else{
+      var fee = value / 10;
+      $('#fee').val(fee);
+    }
+  });
+
+  $('#weight').on('input',function(){
     var value = $(this).val();
     if(value > 9999999){
       $('#profit').val('-');
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,6 +9,7 @@ class PostsController < ApplicationController
 
   def new
     @post = Post.new
+    @posts = Post.all
   end
 
   def create
