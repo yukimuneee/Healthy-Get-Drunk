627225569d3269e1e88c0a21f7ecf94e21362390
yukimuneee <norugbynolife9@icloud.com>
2020-01-31 23:24:46 +0900

    setting

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -15,7 +15,9 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
     post = current_user.posts.current_month
+
     gon.total_m = post.sum(:expence)
+    
     gon.number_m = post.count(:expence)
     gon.total_w = post.sum(:latest_weight)
     gon.number_w = post.count(:latest_weight)
